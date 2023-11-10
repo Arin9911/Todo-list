e808ea53e7ce86e46026d8d504c4ccc3bb348b05
Arin9911 <79415595+Arin9911@users.noreply.github.com>
2023-11-10 19:30:31 +0530

    Create TodoController.java

---
+++ TodoController.java
@@ -0,0 +1,36 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todos")
+public class TodoController {
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @GetMapping
+    public List<Todo> getAllTodos() {
+        return todoRepository.findAll();
+    }
+
+    @PostMapping
+    public Todo createTodo(@RequestBody Todo todo) {
+        return todoRepository.save(todo);
+    }
+
+    @PutMapping("/{id}")
+    public Todo updateTodo(@PathVariable Long id, @RequestBody Todo todo) {
+        if (!todoRepository.existsById(id)) {
+            throw new RuntimeException("Todo not found with id: " + id);
+        }
+        todo.setId(id);
+        return todoRepository.save(todo);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTodo(@PathVariable Long id) {
+        todoRepository.deleteById(id);
+    }
+}
